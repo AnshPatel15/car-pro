f4a38d397084f8969971cca2229d36b554bdfdc0
AnshPatel15 <anjalasnsh@gmail.com>
2023-06-10 16:57:44 +0530

    searchauto error

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,10 @@ export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
+
+export interface SearchManuProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
